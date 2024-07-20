1b3bcb655e3b95bce6b7e293cfe11e1d83f992d4
weiz <azhao.1981@gmail.com>
2024-07-21 03:19:28 +0800

    main

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ def main():
   response = requests.get('https://ifconfig.me')
   print(response.text)
 
+def get_ip():
+  return get_url('https://ifconfig.me')
+
+def get_url(url):
+    try:
+        response = requests.get(url, timeout=3)  # 设置3秒超时
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(e)
+        return ""
+    
 if __name__ == "__main__":
-  main()
+  print(get_ip())
